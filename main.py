77b0c2fd2058ffcae558bb1fb968e6a953cecf0c
damianokpala <110637817+damianokpala@users.noreply.github.com>
2023-06-30 23:14:59 +0000

    Added rows

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from prettytable import PrettyTable
 
 x = PrettyTable()
 
+x.field_names = ["Names", "Reg Number", "Department", "Faculty"]
+x.add_rows(
+  [
+    ["Damian", "20201208492", "Computer Science", "School of information and communication technology"],
+    ["Okpala", "20202636264", "Computer Science", "School of information and communication technology"]
+  ]
+)
 print(x)
